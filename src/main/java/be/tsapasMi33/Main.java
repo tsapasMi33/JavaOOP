1c4b352ca41baa6441a0c5782a9352462b73cfa2
tsapasMi33 <tsapas_mi@hotmail.com>
2023-07-16 21:59:52 +0200

    added

--- src/main/java/be/tsapasMi33/Main.java
+++ src/main/java/be/tsapasMi33/Main.java
@@ -1,10 +1,46 @@
 package be.tsapasMi33;
 
+import be.tsapasMi33.demo.basicClass.DayOfTheWeek;
+import be.tsapasMi33.demo.basicClass.Topping;
 import be.tsapasMi33.exercises.carrace.Race;
+
+import java.util.Random;
 
 
 public class Main {
     public static void main(String[] args) {
-        new Race();
+        DayOfTheWeek weekday = DayOfTheWeek.TUES;
+        System.out.println(weekday);
+
+        for (int i = 0; i < 10; i++) {
+            weekday = getRandomDay();/*
+            System.out.printf("Name is %s, Ordinal Value = %d%n", weekday.name(), weekday.ordinal());
+
+            if (weekday == DayOfTheWeek.FRI) {
+                System.out.println("Found a Friday!");
+            }*/
+
+            switchDay(weekday);
+        }
+
+        for (Topping topping : Topping.values()) {
+            System.out.println(topping.name() + " : " + topping.getPrice());
         }
     }
+
+    public static DayOfTheWeek getRandomDay() {
+        int randomInteger = new Random().nextInt(7);
+        var allDays = DayOfTheWeek.values();
+
+        return allDays[randomInteger];
+    }
+
+    public static void switchDay(DayOfTheWeek weekday){
+        int weekDayInteger = weekday.ordinal() + 1;
+        switch (weekday) {
+            case WED -> System.out.println("Wednesday is Day " + weekDayInteger);
+            case SAT -> System.out.println("Saturday is Day " + weekDayInteger);
+            default -> System.out.println(weekday.name().charAt(0) + weekday.name().substring(1).toLowerCase() + "day is Day " + weekDayInteger);
+        }
+    }
+}
---
+++ src/main/java/be/tsapasMi33/demo/basicClass/DayOfTheWeek.java
@@ -0,0 +1,5 @@
+package be.tsapasMi33.demo.basicClass;
+
+public enum DayOfTheWeek {
+    SUN, MON, TUES, WED, THURS, FRI, SAT
+}
