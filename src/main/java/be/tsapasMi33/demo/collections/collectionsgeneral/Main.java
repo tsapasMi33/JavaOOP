169eeb162dee03ea5b103069ca718f579c722c95
tsapasMi33 <tsapas_mi@hotmail.com>
2023-07-30 17:17:25 +0200

    added

---
+++ src/main/java/be/tsapasMi33/demo/collections/collectionsgeneral/Main.java
@@ -0,0 +1,26 @@
+package be.tsapasMi33.demo.collections.collectionsgeneral;
+
+import be.tsapasMi33.exercises.genericschallenge.Line;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Collection<String> list = new HashSet<>();
+
+        String[] names = {"Anna", "Bob", "Carol", "David", "Edna"};
+        list.addAll(Arrays.asList(names));
+        System.out.println(list);
+
+        list.add("Fred");
+        list.addAll(Arrays.asList("George","Gary", "Grace"));
+        System.out.println(list);
+        System.out.println("Is Gary in the list? " + list.contains("Gary"));
+
+        list.removeIf(s -> s.charAt(0) == 'G');
+        System.out.println(list);
+        System.out.println("Is Gary in the list? " + list.contains("Gary"));
+
+
+    }
+}
---
+++ src/main/java/be/tsapasMi33/demo/collections/deckofcards/Main.java
@@ -0,0 +1,10 @@
+package be.tsapasMi33.demo.collections.deckofcards;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Card> deck = Card.getStandardDeck();
+        Card.printDeck(deck);
+    }
+}
