1a2e9b4305ed68b60d5701cbff1d750ec37bbfd4
tsapasMi33 <tsapas_mi@hotmail.com>
2023-08-07 20:17:32 +0200

    added

---
+++ src/main/java/be/tsapasMi33/exercises/collectionsfullchallenge/Category.java
@@ -0,0 +1,5 @@
+package be.tsapasMi33.exercises.collectionsfullchallenge;
+
+public enum Category {
+    PRODUCE, DAIRY, CEREAL, MEAT, BEVERAGE
+}
---
+++ src/main/java/be/tsapasMi33/exercises/collectionsfullchallenge/Product.java
@@ -0,0 +1,4 @@
+package be.tsapasMi33.exercises.collectionsfullchallenge;
+
+public record Product(String sku, String name, String mfgr, Category category) {
+}
